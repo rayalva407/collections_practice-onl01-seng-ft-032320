25bd4b1883128400e2b3e6cbde52b095e4b098fd
Ray Alva <rayalva407@gmail.com>
2020-03-30 17:27:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| (x.length <=> y.length)}
+end
+
+def swap_elements(array)
+  array.sort {|x,y| (x[1] <=> y[2])}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.map do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
+
+
+
+
+
+
+
 

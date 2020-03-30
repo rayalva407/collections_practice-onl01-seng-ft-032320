f5ea329115b40e67a4cd82a323821270a97d55be
Ray Alva <rayalva407@gmail.com>
2020-03-30 17:07:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| (x.length <=> y.length)}
+end
+
+def swap_elements(array)
+  array.sort {|x,y| (x[1] <=> y[2])}
+end
+
+def reverse_array(array)
+  new_array = []
+  
+end
